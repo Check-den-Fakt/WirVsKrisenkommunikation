94eb2d94072c01bab3ecca307ed629af10f4ddf4
dip <dip@viadee.de>
2020-03-21 11:12:51 +0100

    Initialprojekt Trusted Publisher

---
+++ backend/trustedpublisher/src/main/java/de/hackathon/fakenews/backend/trustedpublisher/TrustedpublisherApplication.java
@@ -0,0 +1,13 @@
+package de.hackathon.fakenews.backend.trustedpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustedpublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustedpublisherApplication.class, args);
+	}
+
+}
