bac35447c246a4808a2fb7866d6fd64449a09dc4
Uladzislau Ulasenka <uladzislau.ulasenka@sva.de>
2020-03-21 14:02:07 +0100

    add result page

--- frontend/src/components/NavMenu.js
+++ frontend/src/components/NavMenu.js
@@ -14,6 +14,7 @@ export class NavMenu extends Component {
         <Nav.Link href="/">Home</Nav.Link>
         <Nav.Link href="/about">About</Nav.Link>
         <Nav.Link href="/check">Check</Nav.Link>
+        <Nav.Link href="/result">Result</Nav.Link>
       </Nav>
       {/*<Form inline>
         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
