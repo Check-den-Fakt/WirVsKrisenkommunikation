f3744accbc35493cf38b5900b783136810ce94ac
kolmio23 <kolmio23@users.noreply.github.com>
2020-03-21 17:03:53 +0100

    add hyscore call with test request and test response

--- backend/known_fakes/src/main.py
+++ backend/known_fakes/src/main.py
@@ -1,9 +1,12 @@
 from flask import Flask, escape, request
 import os
 import json
+import requests
+import pprint
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 app = Flask(__name__)
+pp = pprint.PrettyPrinter(indent=4)
 
 @app.route('/')
 def index():
@@ -16,9 +19,10 @@ def health():
 @app.route('/check', methods=['POST'])
 def textcheck():
     request_data = request.get_json(force=True)
+    #uri:string, text:string
 
     # TODO change to correct request schema
-    check_text_with_hyscore(request_data["mydata"])
+    check_text_with_hyscore(request_data["text"])
 
     # TODO change to correct response schema
     response_data = {"status":"ok"}
@@ -29,6 +33,50 @@ def textcheck():
 def check_text_with_hyscore(text):
     api_url = "https://api.hyscore.io/v3/text"
     HYSCORE_API_KEY = os.getenv("HYSCORE_API_KEY")
-    print(f"search for {text}")
-    # TODO call API with text
+    headers = {'Authorization': 'Basic Og==', 'Content-Type':'application/json','x-api-key':HYSCORE_API_KEY}
 
+    print(f"search for {text}")
+    r = requests.post(api_url, json = {"text":text, "lang":"DE"}, headers=headers)
+    print(pp.pprint(r.json()))
+
+
+# sample text result
+# {   'result': {   'categories': {   'hyscore': [   {   'name': 'health_misc',
+#                                                        'weight': 43.983},
+#                                                    {   'name': 'politics_misc',
+#                                                        'weight': 39.669},
+#                                                    {   'name': 'health',
+#                                                        'weight': 38.182},
+#                                                    {   'name': 'politics',
+#                                                        'weight': 34.437}],
+#                                     'iab': [   {   'category': 'Health & '
+#                                                                'Fitness',
+#                                                    'v1': 'IAB7',
+#                                                    'v2': 286,
+#                                                    'v2-parent': 286,
+#                                                    'weight': 43.983},
+#                                                {   'category': 'Law, '
+#                                                                'Government, & '
+#                                                                'Politics',
+#                                                    'v1': 'IAB11-4',
+#                                                    'v2': 386,
+#                                                    'v2-parent': 379,
+#                                                    'weight': 39.669},
+#                                                {   'category': 'Health & '
+#                                                                'Fitness',
+#                                                    'v1': 'IAB7',
+#                                                    'v2': 286,
+#                                                    'v2-parent': 286,
+#                                                    'weight': 38.182},
+#                                                {   'category': 'Law, '
+#                                                                'Government, & '
+#                                                                'Politics',
+#                                                    'v1': 'IAB11-4',
+#                                                    'v2': 386,
+#                                                    'v2-parent': 379,
+#                                                    'weight': 34.437}]},
+#                   'debug': 'Text field deactivated. ',
+#                   'language': 'DE',
+#                   'readTime': '00:07',
+#                   'textLength': 201},
+#     'status': {'code': 1, 'message': 'All seems well.', 'type': 'Ok'}}
