25042da3c36f39b5f5832c3575bdcc41b35a9443
Uladzislau Ulasenka <uladzislau.ulasenka@sva.de>
2020-03-21 10:43:38 +0100

    add add news page

---
+++ frontend/src/pages/Check/Check.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { AddNews } from '../../components/AddNews';
+
+export default function Check() {
+  // Declare a new state variable, which we'll call "count"
+  return (
+    <div>
+      <p>About Page</p>
+      <AddNews />
+    </div>
+  );
+}
