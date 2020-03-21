9085a0169e097fb09922cebaa05a07574840d7ee
dip <dip@viadee.de>
2020-03-21 13:08:07 +0100

    Trusted Publisher - RequestBody

--- backend/trustedpublisher/src/main/java/de/hackathon/fakenews/backend/trustedpublisher/controller/TrustedPublisherController.java
+++ backend/trustedpublisher/src/main/java/de/hackathon/fakenews/backend/trustedpublisher/controller/TrustedPublisherController.java
@@ -3,13 +3,14 @@ package de.hackathon.fakenews.backend.trustedpublisher.controller;
 import de.hackathon.fakenews.backend.trustedpublisher.domain.TrustedAnswerDTO;
 import de.hackathon.fakenews.backend.trustedpublisher.domain.TrustedPublisherDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrustedPublisherController {
 
     @GetMapping
-    public TrustedAnswerDTO getTrustScore(TrustedPublisherDTO trustedPublisherDTO){
+    public TrustedAnswerDTO getTrustScore(@RequestBody TrustedPublisherDTO trustedPublisherDTO){
 
         TrustedAnswerDTO trustedAnswerDTO = new TrustedAnswerDTO();
 
