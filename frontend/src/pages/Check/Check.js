a7fa0dce56acabc613575083aacb90aa4fb1140f
EvilSad <W1w2w3w4w>
2020-03-21 13:50:10 +0100

    Update Kuchendiagram

--- frontend/src/pages/Check/Check.js
+++ frontend/src/pages/Check/Check.js
@@ -6,9 +6,10 @@ export default function Check() {
   // Declare a new state variable, which we'll call "count"
   return (
     <div>
+        <h1></h1>
+        <Diagram />
       <h1 className="display-4">Check deine Corona-News</h1>
       <p className="lead">Bevor du Nachrichten glaubst oder weiterleitest, überprüfe erst ob sie wahr sind!</p>
-      <Diagram />
       <AddNews />
     </div>
   );
