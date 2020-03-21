505cfbd4a704d66e40b4d9a2d68147e65480cbe6
EvilSad <W1w2w3w4w>
2020-03-21 13:14:37 +0100

    Test

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+
+
 export default function About() {
   // Declare a new state variable, which we'll call "count"
   return (
--- frontend/src/pages/Check/Check.js
+++ frontend/src/pages/Check/Check.js
@@ -5,7 +5,8 @@ export default function Check() {
   // Declare a new state variable, which we'll call "count"
   return (
     <div>
-      <p>About Page</p>
+      <h1 className="display-4">Check deine Corona-News</h1>
+        <p className="lead">Bevor du Nachrichten glaubst oder weiterleitest, überprüfe erst ob sie wahr sind!</p>
       <AddNews />
     </div>
   );
