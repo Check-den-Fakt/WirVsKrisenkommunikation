2c47559aaa9ec7ff729673eb150fbbad08462158
Uladzislau Ulasenka <uladzislau.ulasenka@sva.de>
2020-03-21 21:13:12 +0100

    add report init code

--- frontend/src/pages/Report/Report.js
+++ frontend/src/pages/Report/Report.js
@@ -1,11 +1,36 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './Report.css';
+import { Form } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 
-export default function Report() {
-  // Declare a new state variable, which we'll call "count"
-  return (
+export default class Report extends Component {
+  render () {
+    const { text } = this.state; 
+    // Declare a new state variable, which we'll call "count"
+    return (
     <div>
-      Report Page
+      <h1 className="display-4">Fake-News reporten</h1>
+      <p className="lead">Du hast ein eindeutigen Fake endeckt? Reiche diesen ein!</p>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label></Form.Label>
+          <Form.Control
+            onChange={(e, ee) => this.onChangeValue('text', e, ee)} 
+            as="textarea" 
+            rows="10"
+            placeholder="Füge hier eine URL oder Textnachricht ein"
+          />
+        </Form.Group>
+        <p className="lead">Warum glaubst du, dass das Fake ist? Hast du dafür Belege?</p>
+        <Button 
+          disabled={!text} 
+          onClick={this.handleSubmit} 
+          variant="primary"
+        >
+          Report
+        </Button>
+      </Form>
     </div>
-  );
+    );
+  }
 }
--- frontend/src/pages/Result/Result.js
+++ frontend/src/pages/Result/Result.js
@@ -6,19 +6,32 @@ import { ProgressBar } from 'react-bootstrap';
 
 export default function Result({ result }) {
   // Declare a new state variable, which we'll call "count"
+  const isPositive = false;
+  let content = null;
+  if (isPositive) {
+    content = <>
+      <h1 className="display-4">Diese Nachricht ist mit großer wahrscheinlichkeit OK</h1>
+      <p className="lead pink">Diese Nachricht is verifiziert... blablabal durch was, wen, wie?</p>      
+    </>;
+  } else {
+    content = <>
+      <h1 className="display-4">Diese Nachricht scheint ein Fake zu sein</h1>
+      <p className="lead pink">Diese Nachricht entspricht nicht der Wahrheit... blablabla</p>
+    </>
+  }
+
   return (
     <div>
-        <h1></h1>
-        <div className="container">
-            <div className="raw">
-                <div className="text-center">
-                    <Diagram />
-                </div>
+      <h1></h1>
+      <div className="container">
+        <div className="raw">
+            <div className="text-center">
+                <Diagram />
             </div>
         </div>
-        <h1></h1>
-      <h1 className="display-4">Check deine Corona-News</h1>
-      <p className="lead">Bevor du Nachrichten glaubst oder weiterleitest, überprüfe erst ob sie wahr sind!</p>
+      </div>
+      <h1></h1>
+      {content}
         {/*<ProgressBar now={60} />
       {JSON.stringify(result, null, 2)}
       <div className="text-center">
