b208b36197dc465c03082740703fde6dd758dc76
Julian Hüpppauff <julian@hueppauff.info>
2020-03-22 15:27:59 +0100

    removed top

--- backend/SearchFunction/SearchFunction/Search.cs
+++ backend/SearchFunction/SearchFunction/Search.cs
@@ -33,8 +33,7 @@ namespace SearchFunction
 
             SearchParameters parameters = new SearchParameters()
             {
-                SearchFields = new[] { "Content" },
-                Top = 10
+                SearchFields = new[] { "Content" }
             };
 
             var result = await indexClient.Documents.SearchAsync(requestData.query, parameters);
