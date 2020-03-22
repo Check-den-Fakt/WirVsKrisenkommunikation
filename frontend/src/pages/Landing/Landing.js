835ac820e19707f320166f3ce19ea300a8f6fc4b
PAT_Samuel_Mauch <samuel.mauch@linkit.tech>
2020-03-22 04:06:42 +0100

    landing page content

--- frontend/src/pages/Landing/Landing.js
+++ frontend/src/pages/Landing/Landing.js
@@ -11,47 +11,78 @@ export default function Landing() {
           <img src="/img/logo.svg" width="40%" height="40%"/>
         </div>
 
-        <h1 className="display-4">Finde und widerlege Corona-Fake-News</h1>
+        <h1>Finde und widerlege Corona-Falschnachrichten</h1>
+        <p>Die Anzahl an Falschmeldungen zum Coronavirus steigt immer weiter an. Viele Menschen in Deutschland* haben Schwierigkeiten, den Wahrheitsgehalt dieser Nachrichten zu bewerten*. Dies kann zu Unsicherheiten und Fehlverhalten führen – manchmal mit dramatischen Folgen. </p>
+        <div>
 
-        
+          
         <Button href="/check" variant="primary" size="lg" block>
           Nachricht überprüfen
         </Button>
         <Button href="/report" variant="secondary" size="lg" block>
           Falschnachricht melden
         </Button>
-        
+        </div>
 
-
-
-        <div className="row d-flex justify-content-center">
+        <div className="pt-5 d-flex justify-content-center">
         <div className="polygon background-color-1">
-        <h2 className="display-4">Worum geht’s hier?</h2>
-        <p>Die Anzahl an Falschmeldungen zum Coronavirus steigt immer weiter an. Viele Menschen in Deutschland* haben Schwierigkeiten, den Wahrheitsgehalt dieser Nachrichten zu bewerten*. Dies kann zu Unsicherheiten und Fehlverhalten führen – manchmal mit dramatischen Folgen.</p>
+        <h2 className="display-4">Wer sind wir?</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+        </div>
+
+        <h1>So prüfst Du Deine Nachricht?</h1>
+        <div className="row">
+        <div className="row">
+          <div className="col-3">
+          <span class="material-icons">
+          backup
+          </span>
+          </div>
+        <div className="col-9">
+        <h3>1. Nachricht hochladen</h3>
+        <p>Gib deine Nachricht bei Check the Fact ein. Folgende Möglichkeiten hast du:</p>
+        <ul >
+          <li>Links</li>
+          <li>Freitexteingabe</li>
+          <li>Social Messenger Nachrichten</li>
+          <li>Tweets</li>
+          <li>Dokumente</li>
+          <li>Bilder</li>
+          </ul> 
+        </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+          <span class="material-icons">
+          thumb_up_alt
+          </span>
+          </div>
+        <div className="col-9">
+        <h3>2. Ergebnis erhalten</h3>
+        <p>Nach dem Hochladen erhältst du die Auswertung, mit folgenden Möglichkeit: 
+          Grün: Glaubwürdig. Teilen erwünscht! 
+          Gelb: Zweifelhaft! Hinweise beachten! 
+          Rot: Unglaubwürdig. Nicht weitergeben! Klarstellen! 
+          Grau: Nicht auswertbar. Hinweise beachten!
+          </p>
+        </div>
+        </div>
+        <div className="row">
+          <div className="col-3">
+          <span class="material-icons">
+          share
+          </span>
+          </div>
+        <div className="col-9">
+        <h3>3. Auswertung weiterleiten </h3>
+        <p>Teile das Prüfergebnis mit deinen Kontakten, um sie zu informieren. 
+          </p>
+        </div>
         </div>
         </div>
 
         
-       
-         <h2>Worum geht's hier?</h2>
-         <p className="lead">
-             Die Flut falscher Nachrichten zum <u><i>Coronavirus</i></u> verwirrt uns und macht Angst, denn falsche Informationen sind selbst ein Virus.
-             <br/>
-             <br/>
-             Auf dieser Webseite kannst Du <u>Corona-Meldungen auf Ihren Wahrheitsgehalt prüfen*</u> und von Dir entdeckte Falschmeldungen mitteilen.
-             <br/>
-             <br/>
-             Hilf uns Corona Fake-News zu finden und zu widerlegen.
-             <br/>
-             <br/>
-             Bist Du dir bei einer Nachricht nicht sicher, reiche sie bei uns ein. Wir überprüfen ihren Wahrheitsgehalt. Dadurch helfen wir dir, vertrauenswürdige Nachrichten zu teilen und Falschmeldungen für andere erkennbar zu machen.
-             <br/>
-             <br/>
-             Oder Du greifst uns unter die Arme: Wenn Du eine Falschmeldung gefunden hast, kannst Du sie hier bei uns teilen und uns Gegenbeweise liefern. Wir gleichen alles ab und nehmen die Meldung in unsere Übersicht von Falschnachrichten auf.
-             <br/>
-             <br/>
-             Fake-News verlieren ihre Macht, weil Du sie nicht mehr teilst. Schau genau hin und bilde dir deine eigene Meinung.
-         </p>
 
 
       </>
